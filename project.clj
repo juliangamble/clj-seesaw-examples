b80274e59dbdae6b49885cc8b05fdd8cf2aaa6b7
Julian Gamble <mail@juliangamble.com>
2012-01-23 15:24:09 +1100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-seesaw-examples "1.0.0-SNAPSHOT"
+  :description "Seesaw Examples"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [seesaw "1.3.0"]]
+ 	:source-path "src"
+	:main seesaw.test.examples.clock
+)
---
+++ sign.cmd
@@ -0,0 +1,1 @@
+jarsigner -keystore compstore -storepass sunshine -keypass sunshine -signedjar clj-seesaw-examples.jar clj-seesaw-examples-1.0.0-SNAPSHOT-standalone.jar signFiles
